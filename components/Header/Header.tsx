b71622acb187ae6ca8886da863d28b11aa4c09cf
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-02-05 15:37:00 +0200

    -

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Reactfrom 'react';
+import React from 'react';
 import Logo from '../../public/main/header_logo.svg'
 import Image from "next/image";
 import BurgerClose from '../../public/icons/close.svg'
